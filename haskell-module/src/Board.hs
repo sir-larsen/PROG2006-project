a6ea74366cf2508d2a2e116dd4e720ea9bad40dd
sander fuhr <sandef@stud.ntnu.no>
2021-05-09 15:27:17 +0200

    Refactor

--- haskell-module/src/Board.hs
+++ haskell-module/src/Board.hs
@@ -14,51 +14,52 @@ data Board  = Con [[Player]] (Row, Column)
         deriving (Eq, Show)
 
 
--- ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+-- Functions exclusively for the board ----------------------------------------------------------------
 
 -- creates new board
-newBoard:: (Row, Column) -> Board
-newBoard (r, c) = Con (replicate c []) (r, c)
+createBoard:: (Row, Column) -> Board
+createBoard (r, c) = Con (replicate c []) (r, c)
 
 -- tranposes the board
-transboard :: Board -> Board
-transboard y@(Con x (r,c)) = p2b ( transpose ( b2p y)) r c
+transposer :: Board -> Board
+transposer y@(Con x (r,c)) = playerToBoard ( transpose ( boardToPlayer y)) r c
 
 -- generates array of players from board
-b2p :: Board -> [[Player]]
-b2p (Con x (r, c)) = x
+boardToPlayer :: Board -> [[Player]]
+boardToPlayer (Con x (r, c)) = x
 
 -- generates board from array of players
-p2b :: [[Player]] -> Row -> Column -> Board
-p2b x r c = (Con x (r,c))
+playerToBoard :: [[Player]] -> Row -> Column -> Board
+playerToBoard x r c = (Con x (r,c))
 
 
 -- convert board to string
 --
-showBoard:: Board -> String
-showBoard b@(Con x (r,c)) = (whr b 1) ++ trailer (c-1)
+BoardToString:: Board -> String
+BoardToString b@(Con x (r,c)) = (joinRows b 1) ++ generateLines (c-1)
 
 -- generates the trailing lines for the output
-trailer :: Int -> String
-trailer a | a == -1 = "+\n"
-    | otherwise = "+-" ++ trailer (a-1) ++ " " ++ (show a)
+generateLines :: Int -> String
+generateLines a | a == -1 = "+\n"
+    | otherwise = "+-" ++ generateLines (a-1) ++ " " ++ (show a)
 
 
 --creates output for each cell
-wh1 :: [Player] -> Int -> String
-wh1 [] p = " |"
-wh1 x 1 | head x ==Black = "#|"
+createOutput :: [Player] -> Int -> String
+createOutput [] p = " |"
+createOutput x 1 | head x ==Black = "#|"
   | head x ==White = "o|"
   | otherwise = " |"
-wh1 x p | p > (length x) = " |"
-  | otherwise = (wh1 (tail x) (p-1))
+createOutput x p | p > (length x) = " |"
+  | otherwise = (createOutput (tail x) (p-1))
 
 --seperated the cells of one row
-wh :: [[Player]] -> Int -> String
-wh [x] p = wh1 x p
-wh (x:t) p = wh1 x p ++ wh t p
+separateCells :: [[Player]] -> Int -> String
+separateCells [x] p = createOutput x p
+separateCells (x:t) p = createOutput x p ++ separateCells t p
 
 -- joins the rows together, original table is created from columns !
-whr :: Board -> Int -> String
-whr (Con x (r, c)) p | p >= (c-1) = "|" ++ wh x p ++ "\n"
-         | otherwise = whr (Con x (r, c)) (p+1) ++ "|"  ++ wh x p ++ "\n"
+joinRows :: Board -> Int -> String
+joinRows (Con x (r, c)) p | p >= (c-1) = "|" ++ seperateCells x p ++ "\n"
+         | otherwise = joinRows (Con x (r, c)) (p+1) ++ "|"  ++ seperateCells x p ++ "\n"
+
